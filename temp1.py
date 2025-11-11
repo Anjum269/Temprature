e848e2dbbbad4b5653c289226201b3827efa642b
Anjum269 <01fe24bca044.kletech@gmail.com>
2025-11-11 11:21:35 +0530

    Update and rename temp.py to temp1.py

---
+++ temp1.py
@@ -0,0 +1,14 @@
+ import sys
+
+# check if argument is given
+if len(sys.argv) < 2:
+    print("Please give temperature as argument.")
+else:
+    temp = float(sys.argv[1])
+
+    if temp < 15:
+        print("Cold")
+    elif temp <= 30:
+        print("Normal")
+    else:
+        print("Hot")
